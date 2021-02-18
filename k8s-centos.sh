8bdc4980d3cf3278da40c9dca99c3d5ae29da0eb
eyuriev <45966673+eyuriev@users.noreply.github.com>
2021-02-18 22:28:46 +0200

    Create k8s-centos.sh

---
+++ k8s-centos.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+##
+## Run this script under root
+##
+
+yum -y update
+yum -y install net-tools wget telnet yum-utils device-mapper-persistent-data lvm2
+
+## Add the Docker repository
+sudo yum-config-manager --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo -y
+
+# Install Docker CE
+sudo yum update -y && sudo yum install -y \
+  containerd.io \
+  docker-ce \
+  docker-ce-cli
+
+## Create /etc/docker
+sudo mkdir /etc/docker
+
+# Set up the Docker daemon
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+# Create /etc/systemd/system/docker.service.d
+sudo mkdir -p /etc/systemd/system/docker.service.d
+
+# Restart Docker
+systemctl daemon-reload
+systemctl enable docker
+systemctl restart docker
+
+# Disable swap
+swapoff -a
+sed -i 's/^\(.*swap.*\)$/#\1/' /etc/fstab 
+
+
+# Installing kubeadm
+# https://kubernetes.io/docs/setup/production-environment/tools/kubeadm/install-kubeadm/
+# load netfilter probe specifically
+modprobe br_netfilter
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Enable IP Forwarding
+echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
+cat <<EOF >  /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sysctl --system
+
+# Install kuberentes packages
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# In our case install versions like in OCI PROD 1.16.8
+# yum -y install kubectl kubelet kubeadm
+yum -y install kubelet-1.16.8 kubeadm-1.16.8 kubectl-1.16.8
+
+# Restart and enable Kubelet
+systemctl daemon-reload
+systemctl restart kubelet && systemctl enable kubelet
